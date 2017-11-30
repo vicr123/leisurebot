448583c2be663928d4d887c01bbe532968fe2af5
Aren <aren.cllc@gmail.com>
2017-11-30 19:19:47 +0100

    Working cards (thanks blake!)

--- games/dm/commands/roll.js
+++ games/dm/commands/roll.js
@@ -46,7 +46,7 @@ exports.runCommand = function(user, args, msgo, DiscordMonies) {
                 context.fillText(line, x, y);
             }
 
-            registerFont('./fonts/kabel.ttf', { family: 'Kabel' }) //optional, you'll need to have the kabel font in a "fonts" directory for this to work
+            //registerFont('./fonts/kabel.ttf', { family: 'Kabel' }) //optional, you'll need to have the kabel font in a "fonts" directory for this to work
 
             //The color of this card
             ctx.fillStyle = "white";
@@ -57,7 +57,7 @@ exports.runCommand = function(user, args, msgo, DiscordMonies) {
             ctx.fillRect(2, 2, canvas.width - 2, canvas.height - 2);
             ctx.fillStyle = "black";
             ctx.fillRect(5, 5, canvas.width - 5, canvas.height / 6 + 2);
-            ctx.fillStyle = "#4fc3f7"; //custom color here
+            ctx.fillStyle = newField.color; //custom color here
             ctx.fillRect(6, 6, canvas.width - 6, canvas.height / 6);
             ctx.fillStyle = "white";
             ctx.fillRect(canvas.width - 6, 5, canvas.width, canvas.height / 6 + 3);
@@ -68,15 +68,15 @@ exports.runCommand = function(user, args, msgo, DiscordMonies) {
             ctx.textAlign = "center";
             ctx.fillText("TITLE DEEDS", canvas.width / 2, 75);
             ctx.font = "48px Tahoma";
-            ctx.fillText("PROPERTY NAME", canvas.width / 2, 135); //property name here
+            ctx.fillText(newField.name, canvas.width / 2, 135); //property name here
 
             //Renting text
             //With HOTEL
-            ctx.font = "bold 39px Kabel";
+            ctx.font = "bold 39px Contemporary";
             ctx.textAlign = "center";
-            ctx.fillText("RENT M10", canvas.width / 2, 255);
+            ctx.fillText("RENT M"+newField.cost, canvas.width / 2, 255);
             ctx.textAlign = "left";
-            ctx.font = "lighter 38px Kabel";
+            ctx.font = "lighter 38px Contemporary";
             ctx.fillText("With 1 House", 15, 330);
             ctx.fillText("With 2 Houses", 15, 375);
             ctx.fillText("With 3 Houses", 15, 420);
@@ -85,19 +85,19 @@ exports.runCommand = function(user, args, msgo, DiscordMonies) {
 
             //PRICES FOR HOUSES HERE
             ctx.textAlign = "right";
-            ctx.fillText("HOUSE 1 PRICE", canvas.width - 10, 330); //house 1
-            ctx.fillText("HOUSE 2 PRICE", canvas.width - 10, 375); //house 2
-            ctx.fillText("HOUSE 3 PRICE", canvas.width - 10, 420); //house 3
-            ctx.fillText("HOUSE 4 PRICE", canvas.width - 10, 465); //house 4
-            ctx.fillText("HOTEL PRICE", canvas.width - 10, 510); //hotel
+            ctx.fillText("M"+newField.costHouse, canvas.width - 10, 330); //house 1
+            ctx.fillText("M"+newField.costHouse2, canvas.width - 10, 375); //house 2
+            ctx.fillText("M"+newField.costHouse3, canvas.width - 10, 420); //house 3
+            ctx.fillText("M"+newField.costHouse4, canvas.width - 10, 465); //house 4
+            ctx.fillText("M"+newField.costHotel, canvas.width - 10, 510); //hotel
 
-            ctx.font = "bold 33px Kabel";
+            ctx.font = "bold 33px Contemporary";
             ctx.textAlign = "center";
-            ctx.fillText("Mortgage Value  MORTGAGE VALUE", canvas.width / 2, 660); //replace MORTGAGE VALUE with the value
-            ctx.font = "33px Kabel";
-            ctx.fillText("Houses cost HOUSE COST each", canvas.width / 2, 720); //replace HOUSE COST with the value
+            ctx.fillText("Mortgage Value  M"+newField.mortgageCost, canvas.width / 2, 660); //replace MORTGAGE VALUE with the value
+            ctx.font = "33px Contemporary";
+            ctx.fillText("Houses cost M"+newField.houseCost+" each", canvas.width / 2, 720); //replace HOUSE COST with the value
             ctx.textAlign = "center";
-            wrapText(ctx, "Hotels, HOTEL COST each plus 4 houses", 0 + canvas.width / 2, 765, canvas.width - canvas.width / 4, 30); //replace HOTEL COST with the value
+            wrapText(ctx, "Hotels, "+newField.houseCost+" each plus 4 houses", 0 + canvas.width / 2, 765, canvas.width - canvas.width / 4, 30); //replace HOTEL COST with the value
         };
         generate();
 
