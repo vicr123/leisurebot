fd691e357ab8828b48b180d912483eedb03efeaa
Aren <aren.cllc@gmail.com>
2017-12-04 20:29:48 +0100

    [DM] Fix roll & stations

--- games/dm/classes/boarditems/station.js
+++ games/dm/classes/boarditems/station.js
@@ -4,26 +4,26 @@ class Station extends Purchasable {
     super(name, purchaseValue)
   }
   onStep(player) {
-    //TODO: Implement Railroad Cards
     if (this.owner != null) {
+      var owner = this.owner
       var stationCount=0;
       player.Game.Board.Fields.forEach(function(elem) {
         if (elem instanceof Station) {
-          if (elem.owner && elem.owner == this.owner) {
-            stationCount++;;
+          if (elem.owner && elem.owner == owner) {
+            stationCount++;
           }
         }
       })
       if (stationCount == 1) {
         player.modifyCash(-25);
         this.owner.modifyCash(25);
-      } else if (utilityCount == 2) {
+      } else if (stationCount == 2) {
         player.modifyCash(-50);
         this.owner.modifyCash(-50);
-      } else if (utilityCount == 3) {
+      } else if (stationCount == 3) {
         player.modifyCash(-100);
         this.owner.modifyCash(-100);
-      } else if (utilityCount == 4) {
+      } else if (stationCount == 4) {
         player.modifyCash(-200);
         this.owner.modifyCash(-200);
       }
